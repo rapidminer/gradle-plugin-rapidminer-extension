85f8c0cc02c01f592488b0abc352c73953d6148d
Nils Wöhler <ncwoehler@gmail.com>
2014-06-22 14:53:42 +0200

    Initial commit

---
+++ src/main/groovy/RapidMinerExtensionPlugin.groovy
@@ -0,0 +1,13 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project;
+
+
+class RapidMinerExtensionPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project project) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
