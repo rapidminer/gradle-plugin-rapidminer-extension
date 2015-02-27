19c7f681f41bebe2b71cce0242a47f33b6402134
Nils Wöhler <nwoehler@rapidminer.com>
2015-02-27 10:40:33 +0100

    GRADLE-17: sets default wrapper version to 2.3, adds two new configuration properties for running process tests

--- src/main/groovy/com/rapidminer/gradle/ExtensionConfiguration.groovy
+++ src/main/groovy/com/rapidminer/gradle/ExtensionConfiguration.groovy
@@ -27,11 +27,14 @@ public class ExtensionConfiguration {
 	 */
 	public static final String DEFAULT_GROUP = 'com.rapidminer.extension'
 	
-	public static final String DEFAULT_WRAPPER_VERSION = '2.1'
+	public static final String DEFAULT_WRAPPER_VERSION = '2.3'
 
 	String name
 	String namespace
 	String groupId = DEFAULT_GROUP
+	
+	boolean configureProcessTestEnv = true
+	boolean runProcessTests = true
 
 	String vendor = "RapidMiner GmbH"
 	String homepage = "www.rapidminer.com"
